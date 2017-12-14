4efabee4c7cedffd6bfbbac5623efce93f264be5
ahmedshishir <34244450+ahmedshishir@users.noreply.github.com>
2017-12-15 01:57:01 +0600

    Update musicbot.js

--- musicbot.js
+++ musicbot.js
@@ -1,3 +1,82 @@
+
+const Discord = require('discord.js');
+
+
+
+
+const client = new Discord.Client();
+
+
+
+
+
+
+
+client.on('ready', () => {
+
+
+   console.log('I am ready!');
+
+
+});
+
+
+
+
+
+
+
+client.on('message', message => {
+
+
+   if (message.content === 'ping') {
+
+
+   	message.channel.send('PONG!');
+
+
+ 	}
+
+
+});
+
+
+
+
+
+
+
+client.on('message', message => {
+
+
+   if (message.content === 'bing') {
+
+
+   	message.reply('BONG!');
+
+
+ 	}
+
+
+});
+
+
+
+
+
+
+
+// THIS  MUST  BE  THIS  WAY
+
+
+client.login(process.env.BOT_TOKEN);Mzg1MTY1MTE3Nzk0NDE4Njk0.DRRmQg.xime1JCmbuYD_EU1QM8JjnTEhe0
+
+
+
+
+
+
+
 const Discord = require("discord.js");
 const fs = require("fs");
 const ytdl = require("ytdl-core");
